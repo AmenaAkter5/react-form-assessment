4d60e97e30853c1c7eac6be23c43256f64cc145b
unknown <latapata1952@gmail.com>
2022-06-03 03:33:33 +0600

    form style added

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import './Form.css';
 
 
 const Form = () => {
@@ -18,30 +19,43 @@ const Form = () => {
         <div>
             <h1>Please Fill The Form</h1>
 
-            <form onSubmit={handleSubmit(onSubmit)}>
+            <div className='form-div'>
+                <form onSubmit={handleSubmit(onSubmit)}>
 
-                <div>
-                    <label>
-                        <span>Email</span>
-                    </label>
-                    <input type="email" placeholder="Your Email" {...register("email", {
-                        required: {
-                            value: true,
-                            message: 'Email is Required'
-                        },
-                        pattern: {
-                            value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
-                            message: 'Provide a valid Email'
-                        }
-                    })} />
-                    <label>
-                        {errors.email?.type === 'required' && <span>{errors.email?.message}</span>}
-                        {errors.email?.type === 'pattern' && <span>{errors.email?.message}</span>}
-                    </label>
-                </div>
+                    <div>
+                        <label htmlFor="name">Name</label>
+                        <input type="text" id="name" placeholder="Your Name" {...register("name", {
+                            required: {
+                                value: true,
+                                message: 'Name is Required'
+                            }
+                        })} />
+                        <label>
+                            {errors.name?.type === 'required' && <span>{errors.name?.message}</span>}
+                        </label>
+                    </div>
 
-                <input type="submit" value="Submit" />
-            </form>
+                    <div>
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id="email" placeholder="Your Email" {...register("email", {
+                            required: {
+                                value: true,
+                                message: 'Email is Required'
+                            },
+                            pattern: {
+                                value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                                message: 'Provide a valid Email'
+                            }
+                        })} />
+                        <label htmlFor="email">
+                            {errors.email?.type === 'required' && <span>{errors.email?.message}</span>}
+                            {errors.email?.type === 'pattern' && <span>{errors.email?.message}</span>}
+                        </label>
+                    </div>
+
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
         </div>
     );
 };
